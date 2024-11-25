ce01a9e4ed9e8fe08b8efd980c98d51d002721bf
Julian Meyer <julian.meyer@coinbase.com>
2024-11-25 10:32:11 -0800

    WIP

--- crates/trie/db/tests/witness.rs
+++ crates/trie/db/tests/witness.rs
@@ -96,6 +96,57 @@ fn includes_nodes_for_destroyed_storage_nodes() {
 }
 
 #[test]
+fn includes_nodes_for_destroyed_storage_nodes_2() {
+    let factory = create_test_provider_factory();
+    let provider = factory.provider_rw().unwrap();
+
+    let address = Address::random();
+    let hashed_address = keccak256(address);
+
+    let slot = B256::random();
+    let hashed_slot = keccak256(slot);
+
+    let (slot_2, hashed_slot_2) = {
+        loop {
+            let maybe_slot_2 = B256::random();
+
+            // calculate 0-padded address + slot
+            let address_bytes = address.into_array();
+            // pad 12 bytes to start
+            let address_padded: Vec<u8> = [0u8; 12].iter().copied().chain(address_bytes.iter().copied()).collect();
+
+            // concat address + slot
+            let payload = address_padded.iter().copied().chain(maybe_slot_2.iter().copied()).collect::<Vec<u8>>();
+
+            let hashed_slot_2 = keccak256(payload);
+
+            if hashed_slot_2[0] == hashed_slot[0] {
+                break (maybe_slot_2, hashed_slot_2)
+            }
+        }
+    };
+
+    // Insert account and slot into database
+    provider.insert_account_for_hashing([(address, Some(Account::default()))]).unwrap();
+    provider
+        .insert_storage_for_hashing([(address, [StorageEntry { key: slot, value: U256::from(1) }, StorageEntry { key: slot_2, value: U256::from(2) }])])
+        .unwrap();
+
+    let state_root = StateRoot::from_tx(provider.tx_ref()).root().unwrap();
+
+    let witness = TrieWitness::from_tx(provider.tx_ref())
+        .compute(HashedPostState {
+            accounts: HashMap::from([(hashed_address, Some(Account::default()))]),
+            storages: HashMap::from([(hashed_address, HashedStorage::from_iter(false, [(slot, U256::from(1))]))]), // destroyed
+        })
+        .unwrap();
+
+    println!("root: {:?}", state_root);
+    println!("witness: {:?}", witness.contains_key(&slot_2));
+    println!("witness: {:?}", witness);
+}
+
+#[test]
 fn correctly_decodes_branch_node_values() {
     let factory = create_test_provider_factory();
     let provider = factory.provider_rw().unwrap();
